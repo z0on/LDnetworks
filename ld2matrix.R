6cc13a3f0fa35320e56b0fff9446ff65c38bc6ff
Mikhail V Matz <matz@utexas.edu>
2025-11-26 19:24:24 -0600

    Add files via upload

---
+++ ld2matrix.R
@@ -0,0 +1,15 @@
+require(reshape)
+fin = commandArgs(T)
+message("reading input file...")
+ald=read.table(fin,sep="\t")
+# unremark next line if reading un-slimmed LD table from ngsLD
+#ald=read.table(fin,sep="\t")[,c(1,2,7)]
+message("   done")
+names(ald)=c("s1","s2","rEM")
+message("formatting matrix...")
+ldmat=cast(ald,s1~s2)
+message("   done")
+ldmat[upper.tri(ldmat)]=ldmat[lower.tri(ldmat)]
+diag(ldmat)=1
+save(ldmat,file=paste(fin,"_matrix.RData",sep="",collapse=""))
+
